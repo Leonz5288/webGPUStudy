cf89138d189e5b9a100e7b995d8848bd8d6149ed
Leon Zhang <leonz5288@hotmail.com>
2021-08-13 15:44:35 +0800

    add shader code

--- render2D.js
+++ render2D.js
@@ -26,6 +26,32 @@ async function init() {
         }]
     };
 
+    const vertexShaderModule = device.createShaderModule({
+        code:
+        `
+        [[block]] struct VertexOut{
+            [[builtin(position)]] position : vec4<f32>;
+            [[location(0)]] color : vec4<f32>;
+        };
+
+        [[stage(vertex)]]
+        fn vertex_main([[location(0)]] position : vec4<f32>,
+                       [[location(1)]] color : vec4<f32>) -> VertexOut
+        {
+            var output : VertexOut;
+            output.position = position;
+            output.color = color;
+            return output;
+        }
+
+        [[stage(fragment)]]
+        fn fragment_main(fragData: VertexOut) -> [[location(0)]] vec4<f32>
+        {
+            return fragData.color;
+        }
+        `
+    });
+
     const commandEncoder = device.createCommandEncoder();
     const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
     // optional draww commands
